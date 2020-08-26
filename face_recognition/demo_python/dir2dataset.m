7d4846fc40a8666a7ba68890621247fe2a6efb8b
manu <zxthz@126.com>
2020-08-26 16:48:01 +0800

    arcface r50 to rknn (success)

---
+++ face_recognition/demo_python/dir2dataset.m
@@ -0,0 +1,30 @@
+%%
+% author: manu
+
+%%
+close all; clear all;
+
+%%
+opts.dir_img = '/media/manu/intel/workspace/rknn/demo_python/imgs';
+opts.output = '/media/manu/intel/workspace/rknn/demo_python/dataset.txt';
+
+%%
+list_img  = struct2cell(dir(fullfile(opts.dir_img, '*.jpg')))';
+paths_img = fullfile(opts.dir_img, list_img(:, 1));
+
+fileID = fopen(opts.output, 'w');
+
+for i = 1 : length(paths_img)
+    
+    path_img = paths_img{i};
+    
+    fprintf('processing %dth img %s [total %d]\n', ...
+        i, path_img, length(paths_img));
+
+    fprintf(fileID, '%s\n', path_img);
+
+end
+
+fclose(fileID);
+
+%%
