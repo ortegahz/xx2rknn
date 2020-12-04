07d5094601ef6bf1f56586a653e35921402723f7
manu <zxthz@126.com>
2020-12-04 17:10:21 +0800

    build pfc pytorch model (perfect)

--- dir2dataset.m
+++ dir2dataset.m
@@ -5,8 +5,8 @@
 close all; clear all;
 
 %%
-opts.dir_img = '/home/manu/workspace/rknn/face_recognition/demo_python/imgs';
-opts.output = '/home/manu/workspace/rknn/face_recognition/demo_python/dataset.txt';
+opts.dir_img = '/home/manu/workspace/rknn/face_recognition/imgs';
+opts.output = '/home/manu/workspace/rknn/face_recognition/onnx/dataset.txt';
 
 %%
 list_img  = struct2cell(dir(fullfile(opts.dir_img, '*.jpg')))';
