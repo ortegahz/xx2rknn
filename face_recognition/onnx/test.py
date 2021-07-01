c6d13caac9c9e28086bbc54a748bce26aaeba3d6
manu <zxthz@126.com>
2021-07-01 14:25:49 +0800

    convert glint360k_cosface_r100_fp16_0.1_bs1024 perfectly (onnx rknn)

--- face_recognition/onnx/test.py
+++ face_recognition/onnx/test.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
     # Build model
     print('--> Building model')
     # ret = rknn.build(do_quantization=True, dataset='./dataset.txt')
-    ret = rknn.build(do_quantization=True, dataset='/home/manu/tmp/dataset.txt', pre_compile=True)
+    ret = rknn.build(do_quantization=True, dataset='/home/manu/tmp/dataset.txt', pre_compile=False)
     if ret != 0:
         print('Build model failed!')
         exit(ret)
