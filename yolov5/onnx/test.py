7c3b9f8da8e4399c020872ad64a5e71176d5a491
manu <zxthz@126.com>
2021-08-03 18:56:03 +0800

    yolov5 to rknn perfectly (onnx)

--- yolov5/onnx/test.py
+++ yolov5/onnx/test.py
@@ -1,10 +1,18 @@
+import os
+import urllib
+import traceback
+import time
+import sys
 import numpy as np
 import cv2
 from rknn.api import RKNN
 
+ONNX_MODEL = '/media/manu/intel/workspace/yolov5/bestce.onnx'
+RKNN_MODEL = '/home/manu/tmp/best.rknn'
+
 
 def show_outputs(outputs):
-    output = outputs[0].reshape(-1)
+    output = outputs[0][0]
     output_sorted = sorted(output, reverse=True)
     top5_str = 'model\n-----TOP 5-----\n'
     for i in range(5):
@@ -21,11 +29,56 @@ def show_outputs(outputs):
     print(top5_str)
 
 
+def readable_speed(speed):
+    speed_bytes = float(speed)
+    speed_kbytes = speed_bytes / 1024
+    if speed_kbytes > 1024:
+        speed_mbytes = speed_kbytes / 1024
+        if speed_mbytes > 1024:
+            speed_gbytes = speed_mbytes / 1024
+            return "{:.2f} GB/s".format(speed_gbytes)
+        else:
+            return "{:.2f} MB/s".format(speed_mbytes)
+    else:
+        return "{:.2f} KB/s".format(speed_kbytes)
+
+
+def show_progress(blocknum, blocksize, totalsize):
+    speed = (blocknum * blocksize) / (time.time() - start_time)
+    speed_str = " Speed: {}".format(readable_speed(speed))
+    recv_size = blocknum * blocksize
+
+    f = sys.stdout
+    progress = (recv_size / totalsize)
+    progress_str = "{:.2f}%".format(progress * 100)
+    n = round(progress * 50)
+    s = ('#' * n).ljust(50, '-')
+    f.write(progress_str.ljust(8, ' ') + '[' + s + ']' + speed_str)
+    f.flush()
+    f.write('\r\n')
+
+
 if __name__ == '__main__':
 
     # Create RKNN object
     rknn = RKNN()
 
+    # If model does not exist, download it.
+    # Download address:
+    # https://s3.amazonaws.com/onnx-model-zoo/resnet/model/model.onnx
+    if not os.path.exists(ONNX_MODEL):
+        print('--> Download {}'.format(ONNX_MODEL))
+        url = 'https://s3.amazonaws.com/onnx-model-zoo/resnet/model/model.onnx'
+        download_file = ONNX_MODEL
+        try:
+            start_time = time.time()
+            urllib.request.urlretrieve(url, download_file, show_progress)
+        except:
+            print('Download {} failed.'.format(download_file))
+            print(traceback.format_exc())
+            exit(-1)
+        print('done')
+    
     # pre-process config
     print('--> config model')
     # ([0 255] - 0.0) / 255.0 --> [0 1]
@@ -35,18 +88,16 @@ if __name__ == '__main__':
 
     # Load tensorflow model
     print('--> Loading model')
-    ret = rknn.load_caffe(model='/media/manu/intel/workspace/onnx2caffe/model/bestc.prototxt',
-                          proto='caffe',
-                          blobs='/media/manu/intel/workspace/onnx2caffe/model/bestc.caffemodel')
+    ret = rknn.load_onnx(model=ONNX_MODEL)
     if ret != 0:
-        print('Load model failed! Ret = {}'.format(ret))
+        print('Load model failed!')
         exit(ret)
     print('done')
 
     # Build model
     print('--> Building model')
+    # ret = rknn.build(do_quantization=True, dataset='./dataset.txt')
     ret = rknn.build(do_quantization=True, dataset='/home/manu/tmp/dataset.txt', pre_compile=True)
-    # ret = rknn.build(do_quantization=True, dataset='./dataset.txt', pre_compile=False)
     if ret != 0:
         print('Build model failed!')
         exit(ret)
@@ -54,7 +105,7 @@ if __name__ == '__main__':
 
     # Export rknn model
     print('--> Export RKNN model')
-    ret = rknn.export_rknn('/home/manu/tmp/model.rknn')
+    ret = rknn.export_rknn(RKNN_MODEL)
     if ret != 0:
         print('Export model.rknn failed!')
         exit(ret)
@@ -64,9 +115,9 @@ if __name__ == '__main__':
     img = cv2.imread('/media/manu/samsung/pics/126_1152_576.bmp')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+    # init runtime environment
     print('--> Init runtime environment')
     ret = rknn.init_runtime()
-    # ret = rknn.init_runtime(target='rk3399', device_id='TDs33101190500149')
     if ret != 0:
         print('Init runtime environment failed')
         exit(ret)
@@ -82,12 +133,11 @@ if __name__ == '__main__':
         np.savetxt('/home/manu/tmp/rknn_output_%s.txt' % save_i, save_output,
                    fmt="%f", delimiter="\n")
 
+    # x = outputs[0]
+    # output = np.exp(x)/np.sum(np.exp(x))
+    # outputs = [output]
     # show_outputs(outputs)
-    print('done')
 
-    # perf
-    print('--> Begin evaluate model performance')
-    perf_results = rknn.eval_perf(inputs=[img])
     print('done')
 
     rknn.release()
